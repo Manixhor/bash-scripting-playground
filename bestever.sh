b6ef576edb868ff7e848aded50edccb348889dfd
MANI <mani@MANIs-MacBook-Air.local>
2026-04-29 19:44:47 +0530

    initial commit

---
+++ bestever.sh
@@ -0,0 +1,11 @@
+
+name="Mani"
+echo "Hello Good Mrng $name 😊 "
+
+		
+
+sleep 1 
+
+echo "You have some works to do $name!"
+
+
---
+++ hi.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+
+echo "hello love"
+
+sleep 3 
+
+echo "uh uh"
+
+sleep 3
+
+echo "bye"
+
